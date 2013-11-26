51d28068eba295c088ddeeda72eb353000a6fc61
uakf.b <perennate@gmail.com>
2013-11-26 18:35:52 -0500

    Fix default configuration filename, and allow passing filename as argument.

--- src/net/entgaming/wc3connectd/WCServer.java
+++ src/net/entgaming/wc3connectd/WCServer.java
@@ -13,110 +13,116 @@ import net.entgaming.wc3connect.WCUtil;
 
 public class WCServer extends Thread {
 	public static int PORT = 6112;
-	
+
 	DatagramSocket wc3Server;
 	Database database;
-	
+
 	ArrayList<byte[]> trustedHosts;
-	
+
 	public static void main(String args[]) {
-		Config.init("wc3connect.cfg");
-		
+		String configFilename = "wc3cd.cfg";
+
+		if(args.length >= 1) {
+			configFilename = args[0];
+		}
+
+		Config.init(configFilename);
+
 		Database database = new Database();
-		
+
 		WCServer server = new WCServer(database);
 		server.init();
 		server.start();
 	}
-	
+
 	public WCServer(Database database) {
 		this.database = database;
-		
+
 		trustedHosts = new ArrayList<byte[]>();
 		String[] array = Config.getString("trusted", "").split(" ");
-		
+
 		for(String str : array) {
 			str = str.trim();
-			
+
 			if(!str.equals("")) {
 				String[] parts = str.split("\\.");
 				byte[] bytes = new byte[parts.length];
-				
+
 				for(int i = 0; i < parts.length; i++) {
 					bytes[i] = (byte) Integer.parseInt(parts[i]);
 				}
-				
+
 				trustedHosts.add(bytes);
 			}
 		}
 	}
-		
+
 	public void init() {
 		SocketAddress addr = new InetSocketAddress(Config.getInt("port", PORT));
-		
+
 		try {
 			wc3Server = new DatagramSocket(addr);
 		} catch(IOException ioe) {
 			System.out.println("[WCServer] Error while binding UDP: " + ioe.getLocalizedMessage());
 		}
 	}
-		
+
 	public void run() {
 		ByteBuffer buf = ByteBuffer.allocate(65536);
 		byte[] pbuf = new byte[65536];
 		DatagramPacket packet = new DatagramPacket(pbuf, 65536);
-		
+
 		while(true) {
 			if(!wc3Server.isBound()) {
 				try {
 					Thread.sleep(5000);
 				} catch(InterruptedException e) {}
-				
+
 				init();
 			}
-			
+
 			try {
 				wc3Server.receive(packet);
-				
+
 				byte[] address = packet.getAddress().getAddress();
-				
+
 				if(address.length != 4) {
 					continue;
 				}
-				
+
 				boolean trusted = false;
-				
+
 				for(byte[] host : trustedHosts) {
 					if(address.length == host.length) {
 						boolean match = true;
-						
+
 						for(int i = 0; i < host.length; i++) {
 							if(host[i] != address[i]) {
 								match = false;
 								break;
 							}
 						}
-						
+
 						if(match) {
 							trusted = true;
 							break;
 						}
 					}
 				}
-				
+
 				if(!trusted) {
 					System.out.println("[WCServer] Untrusted from " + packet.getAddress().getHostAddress());
 					continue;
 				}
-				
+
 				ByteBuffer lbuf = ByteBuffer.wrap(packet.getData(), packet.getOffset(), packet.getLength());
 				buf.clear();
-				
+
 				lbuf.order(ByteOrder.LITTLE_ENDIAN);
 				buf.order(ByteOrder.LITTLE_ENDIAN);
 
 				if(packet.getLength() < 21) continue;
-				
+
 				int header = WCUtil.unsignedByte(lbuf.get()); //W3GS header constant
 
 				if(header != 247) {
@@ -141,13 +147,13 @@ public class WCServer extends Thread {
 				buf.putInt(lbuf.getInt()); //unknown
 
 				String gamename = WCUtil.getTerminatedString(lbuf);
-				
+
 				byte[] bytes = gamename.getBytes();
 				buf.put(bytes);
 				buf.put((byte) 0); //null terminator
 
 				if(packet.getLength() < 21 + bytes.length + 2) continue;
-				
+
 				buf.put(lbuf.get()); //unknown
 
 				byte[] array = WCUtil.getTerminatedArray(lbuf);
@@ -158,13 +164,13 @@ public class WCServer extends Thread {
 					System.out.println(packet.getLength() + "," + (23 + bytes.length + 2 + array.length + 22));
 					continue;
 				}
-				
+
 				buf.putInt(lbuf.getInt()); //slots total
 				buf.putInt(lbuf.getInt()); //game type
 				buf.putInt(lbuf.getInt()); //unknown
 				buf.putInt(lbuf.getInt()); //slots open
 				buf.putInt(lbuf.getInt()); //up time
-				
+
 				int port = WCUtil.unsignedShort(lbuf.getShort());
 				buf.putShort((short) port); //port
 
